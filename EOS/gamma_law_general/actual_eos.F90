33644e2029293666add328594a1eeefbcf4665b2
Michael Zingale <michael.zingale@stonybrook.edu>
2019-11-04 14:58:29 -0500

    work with the new interfaces

--- EOS/gamma_law_general/actual_eos.F90
+++ EOS/gamma_law_general/actual_eos.F90
@@ -4,7 +4,7 @@
 !
 ! The ratio of specific heats (gamma) is allowed to vary.  NOTE: the
 ! expression for entropy is only valid for an ideal MONATOMIC gas
-! (gamma = 5/3).  
+! (gamma = 5/3).
 
 module actual_eos_module
 
@@ -13,10 +13,11 @@ module actual_eos_module
   use amrex_constants_module
   use network, only: nspec, aion, aion_inv, zion
   use eos_type_module
+  use eos_composition_module, only : eos_comp_t
 
   implicit none
 
-  character (len=64), public :: eos_name = "gamma_law_general"  
+  character (len=64), public :: eos_name = "gamma_law_general"
 
   double precision, allocatable, save :: gamma_const
 
@@ -27,7 +28,7 @@ module actual_eos_module
 #ifdef AMREX_USE_CUDA
   attributes(managed) :: gamma_const, assume_neutral
 #endif
- 
+
 contains
 
   subroutine actual_eos_init
@@ -38,7 +39,7 @@ contains
 
     allocate(gamma_const)
     allocate(assume_neutral)
- 
+
     ! constant ratio of specific heats
     if (eos_gamma .gt. 0.d0) then
        gamma_const = eos_gamma
@@ -49,12 +50,12 @@ contains
     assume_neutral = eos_assume_neutral
 
     !$acc update device(gamma_const, eos_assume_neutral)
-    
+
   end subroutine actual_eos_init
 
 
 
-  subroutine actual_eos(input, state)
+  subroutine actual_eos(input, state, state_comp)
 
     !$acc routine seq
 
@@ -64,6 +65,7 @@ contains
 
     integer,      intent(in   ) :: input
     type (eos_t), intent(inout) :: state
+    type (eos_comp_t), intent(in) :: state_comp
 
     ! Get the mass of a nucleon from Avogadro's number.
     double precision, parameter :: m_nucleon = ONE / n_A
@@ -73,14 +75,7 @@ contains
 
     !$gpu
 
-    ! Calculate mu.
-    
-    if (assume_neutral) then
-       state % mu = state % abar
-    else
-       state % mu = ONE / sum( (ONE + zion(:)) * state % xn(:) * aion_inv(:) )
-    endif
-    
+
     !-------------------------------------------------------------------------
     ! For all EOS input modes EXCEPT eos_input_rt, first compute dens
     ! and temp as needed from the inputs.
@@ -102,16 +97,16 @@ contains
        ! Solve for the temperature:
        ! h = e + p/rho = (p/rho)*[1 + 1/(gamma-1)] = (p/rho)*gamma/(gamma-1)
 
-       state % T = (state % h * state % mu * m_nucleon / k_B)*(gamma_const - ONE)/gamma_const
+       state % T = (state % h * state_comp % mu * m_nucleon / k_B)*(gamma_const - ONE)/gamma_const
 
     case (eos_input_tp)
 
        ! temp, pres, and xmass are inputs
-       
+
        ! Solve for the density:
        ! p = rho k T / (mu m_nucleon)
 
-       state % rho =  state % p * state % mu * m_nucleon / (k_B * state % T)
+       state % rho =  state % p * state_comp % mu * m_nucleon / (k_B * state % T)
 
     case (eos_input_rp)
 
@@ -120,42 +115,42 @@ contains
        ! Solve for the temperature:
        ! p = rho k T / (mu m_nucleon)
 
-       state % T = state % p * state % mu * m_nucleon / (k_B * state % rho)
+       state % T = state % p * state_comp % mu * m_nucleon / (k_B * state % rho)
 
     case (eos_input_re)
 
        ! dens, energy, and xmass are inputs
-       
+
        ! Solve for the temperature
        ! e = k T / [(mu m_nucleon)*(gamma-1)]
 
-       state % T   = state % e * state % mu * m_nucleon * (gamma_const-ONE) / k_B
+       state % T   = state % e * state_comp % mu * m_nucleon * (gamma_const-ONE) / k_B
 
     case (eos_input_ps)
 
        ! pressure, entropy, and xmass are inputs
-       
+
        ! Solve for the temperature
-       ! Invert Sackur-Tetrode eqn (below) using 
+       ! Invert Sackur-Tetrode eqn (below) using
        ! rho = p mu m_nucleon / (k T)
 
        state % T  = state % p**(TWO/FIVE) * &
-                    ( TWO*M_PI*hbar*hbar/(state % mu*m_nucleon) )**(THREE/FIVE) * &
-                    dexp(TWO*state % mu*m_nucleon*state % s/(FIVE*k_B) - ONE) / k_B
+                    ( TWO*M_PI*hbar*hbar/(state_comp % mu * m_nucleon) )**(THREE/FIVE) * &
+                    dexp(TWO * state_comp % mu * m_nucleon*state % s/(FIVE*k_B) - ONE) / k_B
 
        ! Solve for the density
        ! rho = p mu m_nucleon / (k T)
-       
-       state % rho =  state % p * state % mu * m_nucleon / (k_B * state % T)
+
+       state % rho =  state % p * state_comp % mu * m_nucleon / (k_B * state % T)
 
     case (eos_input_ph)
 
        ! pressure, enthalpy and xmass are inputs
-       
+
        ! Solve for temperature and density
 
        state % rho = state % p / state % h * gamma_const / (gamma_const - ONE)
-       state % T   = state % p * state % mu * m_nucleon / (k_B * state % rho)
+       state % T   = state % p * state_comp % mu * m_nucleon / (k_B * state % rho)
 
     case (eos_input_th)
 
@@ -172,20 +167,20 @@ contains
 #ifndef AMREX_USE_GPU
        call amrex_error('EOS: invalid input.')
 #endif
-       
+
     end select
-    
+
     !-------------------------------------------------------------------------
     ! Now we have the density and temperature (and mass fractions /
     ! mu), regardless of the inputs.
     !-------------------------------------------------------------------------
-           
+
     Tinv = ONE / state % T
     rhoinv = ONE / state % rho
 
     ! Compute the pressure simply from the ideal gas law, and the
     ! specific internal energy using the gamma-law EOS relation.
-    state % p = state % rho * state % T * (k_B / (state % mu*m_nucleon))
+    state % p = state % rho * state % T * (k_B / (state_comp % mu * m_nucleon))
     state % e = state % p/(gamma_const - ONE) * rhoinv
 
     ! enthalpy is h = e + p/rho
@@ -193,16 +188,16 @@ contains
 
     ! entropy (per gram) of an ideal monoatomic gas (the Sackur-Tetrode equation)
     ! NOTE: this expression is only valid for gamma = 5/3.
-    state % s = (k_B/(state % mu*m_nucleon))*(2.5_rt + &
-         log( ( (state % mu*m_nucleon)**2.5_rt * rhoinv )*(k_B * state % T)**1.5_rt * fac ) )
+    state % s = (k_B/(state_comp % mu * m_nucleon))*(2.5_rt + &
+         log( ( (state_comp % mu * m_nucleon)**2.5_rt * rhoinv )*(k_B * state % T)**1.5_rt * fac ) )
 
-    ! Compute the thermodynamic derivatives and specific heats 
+    ! Compute the thermodynamic derivatives and specific heats
     state % dpdT = state % p * Tinv
     state % dpdr = state % p * rhoinv
     state % dedT = state % e * Tinv
     state % dedr = ZERO
-    state % dsdT = 1.5_rt * (k_B / (state % mu * m_nucleon)) * Tinv
-    state % dsdr = - (k_B / (state % mu * m_nucleon)) * rhoinv
+    state % dsdT = 1.5_rt * (k_B / (state_comp % mu * m_nucleon)) * Tinv
+    state % dsdr = - (k_B / (state_comp % mu * m_nucleon)) * rhoinv
     state % dhdT = state % dedT + state % dpdT * rhoinv
     state % dhdr = ZERO
 
@@ -224,27 +219,27 @@ contains
     state % cs = sqrt(gamma_const * state % p * rhoinv)
 
 #ifdef EXTRA_THERMO
-    state % dpdA = - state % p * (ONE/state % abar)
-    state % dedA = - state % e * (ONE/state % abar)
+    state % dpdA = - state % p * (ONE/state_comp % abar)
+    state % dedA = - state % e * (ONE/state_comp % abar)
 
     if (assume_neutral) then
       state % dpdZ = ZERO
       state % dedZ = ZERO
     else
-      state % dpdZ = state % p * (ONE/(ONE + state % zbar))
-      state % dedZ = state % e * (ONE/(ONE + state % zbar))
+      state % dpdZ = state % p * (ONE/(ONE + state_comp % zbar))
+      state % dedZ = state % e * (ONE/(ONE + state_comp % zbar))
     endif
 #endif
 
   end subroutine actual_eos
 
   subroutine actual_eos_finalize
-    
+
     implicit none
 
     deallocate(gamma_const)
     deallocate(assume_neutral)
-  
+
   end subroutine actual_eos_finalize
 
 end module actual_eos_module
