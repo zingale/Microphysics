6210a9b94de26b06dead47a55cd70b7ade6857af
Michael Zingale <michael.zingale@stonybrook.edu>
2026-08-28 15:40:09 -0400

    update CNO_extras remove the C+C and C+O rates -- we should not hit those under the conditions where this net is appropriate
    add 3 rate sequence approximations from MESA's cno_extras net that
    connect to Mg22

--- networks/CNO_extras/CNO_extras.py
+++ networks/CNO_extras/CNO_extras.py
@@ -13,6 +13,37 @@ def create_network():
                               tabular_ordering=["ffn", "langanke", "oda"],
                               inert_nuclei=["fe56"], network_type="amrex")
 
+    # remove the C-burning rates
+    rr = net.get_rate_by_name(["c12(c12,a)ne20",
+                               "o16(c12,a)mg24",
+                               "ne20(a,c12)c12",
+                               "mg24(a,c12)o16"])
+
+    net.remove_rates(rr)
+
+    # add additional breakout rates
+    rl = pyna.ReacLibLibrary()
+
+    rne18ag = rl.get_rate_by_name("ne18(a,g)mg22")
+    net.remove_rates(rne18ag)
+
+    rne18_mg22, _ = pyna.rates.make_ap_pg_rates(rl, "ne18", "mg22",
+                                                use_detailed_balance=True)
+
+    rne19pg = rl.get_rate_by_name("ne19(p,g)na20")
+    rne19_mg22 = pyna.ModifiedRate(rne19pg,
+                                   new_products=[pyna.Nucleus("mg22")],
+                                   stoichiometry={pyna.Nucleus("p"): 3},
+                                   description="Ne19(p,γ)Na20(p,γ)Mg21(β+)Na21(p,γ)Mg22")
+
+    rne20pg = rl.get_rate_by_name("ne20(p,g)na21")
+    rne20_mg22 = pyna.ModifiedRate(rne20pg,
+                                   new_products=[pyna.Nucleus("mg22")],
+                                   stoichiometry={pyna.Nucleus("p"): 2},
+                                   description="Ne20(p,γ)Na21(p,γ)Mg22")
+
+    net.add_rates([rne18_mg22, rne19_mg22, rne20_mg22])
+
     return net
 
 
@@ -36,6 +67,7 @@ def doit():
              rotated=True,
              hide_xalpha=True)
 
+    net.summary()
 
 if __name__ == "__main__":
     doit()
