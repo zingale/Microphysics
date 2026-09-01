0f555ff52f46a625aa456dbe68f993e4ed5ea0e0
Michael Zingale <michael.zingale@stonybrook.edu>
2026-09-01 07:38:51 -0400

    update

--- networks/CNO_extras/CNO_extras.py
+++ networks/CNO_extras/CNO_extras.py
@@ -3,47 +3,7 @@ import pynucastro as pyna
 
 def create_network():
 
-    net = pyna.network_helper(["h1", "he4",
-                               "c12", "c13",
-                               "n13", "n14", "n15",
-                               "o14", "o15", "o16", "o17", "o18",
-                               "f17", "f18", "f19",
-                               "ne18", "ne19", "ne20",
-                               "mg22", "mg24"],
-                              tabular_ordering=["ffn", "langanke", "oda"],
-                              inert_nuclei=["fe56"], network_type="amrex")
-
-    # remove the C-burning rates
-    rr = net.get_rate_by_name(["c12(c12,a)ne20",
-                               "o16(c12,a)mg24",
-                               "ne20(a,c12)c12",
-                               "mg24(a,c12)o16"])
-
-    net.remove_rates(rr)
-
-    # add additional breakout rates
-    rl = pyna.ReacLibLibrary()
-
-    rne18ag = rl.get_rate_by_name("ne18(a,g)mg22")
-    net.remove_rates(rne18ag)
-
-    rne18_mg22, _ = pyna.rates.make_ap_pg_rates(rl, "ne18", "mg22",
-                                                use_detailed_balance=True)
-
-    rne19pg = rl.get_rate_by_name("ne19(p,g)na20")
-    rne19_mg22 = pyna.ModifiedRate(rne19pg,
-                                   new_products=[pyna.Nucleus("mg22")],
-                                   stoichiometry={pyna.Nucleus("p"): 3},
-                                   description="Ne19(p,γ)Na20(p,γ)Mg21(β+)Na21(p,γ)Mg22")
-
-    rne20pg = rl.get_rate_by_name("ne20(p,g)na21")
-    rne20_mg22 = pyna.ModifiedRate(rne20pg,
-                                   new_products=[pyna.Nucleus("mg22")],
-                                   stoichiometry={pyna.Nucleus("p"): 2},
-                                   description="Ne20(p,γ)Na21(p,γ)Mg22")
-
-    net.add_rates([rne18_mg22, rne19_mg22, rne20_mg22])
-
+    net = pyna.common_networks.cno(network_type="amrex")
     return net
 
 
